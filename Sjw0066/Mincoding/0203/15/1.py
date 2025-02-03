93b11f0a8131b1d200b0de641dc34f3f25fde0e4
sin jin woo <sjw0066@naver.com>
2025-02-03 22:03:05 +0900

    0203_study

--- Sjw0066/Mincoding/0203/15/1.py
+++ Sjw0066/Mincoding/0203/15/1.py
@@ -1,1 +1,7 @@
-afsadfaasfs
+str1=input()
+str2=input()
+
+if str1==str2:
+    print('같음')
+else:
+    print('다름')
---
+++ Sjw0066/Mincoding/0203/15/9.py
@@ -0,0 +1,7 @@
+str1='BBQWORLD'
+str2='KFCAPPLE'
+str3='LOT'
+
+input1=input()
+
+print(f'{str1.count(input1)+str2.count(input1)+str3.count(input1)}')
