0746032707c024b16979be5a256daa67ef6e857e
김희주 <ajtwlsgmlwn@naver.com>
2025-02-04 22:41:32 +0900

    idontknow

--- huizoo/ming/0203/18.5_9.py
+++ huizoo/ming/0203/18.5_9.py
@@ -2,10 +2,16 @@ st = list(input())
 dat = [0]*26
 for char in st : 
     dat[ord(char)-65] += 1
-# for i in range(26) :
-#     while dat[i] >= 2 : 
-#         st.remove(chr(i+65))
-#         dat[i] -= 1
-# print(''.join(sorted(st)))
-st = [char for char in st if dat[ord(char) -65] < 2]
+for i in range(26) :
+    while dat[i] >= 2 : 
+        st.remove(chr(i+65))
+        dat[i] -= 1
+
+# result =[]
+# for char in st : 
+#     if dat[ord(char) -65] > 0 :
+#         result.append(char)
+#         dat[ord(char)-65] = 0
+
 print(''.join(sorted(st)))
+
