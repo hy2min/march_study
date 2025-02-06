cf281bae3cb5d1166296f2f23cbf36bf8a22b41d
sin jin woo <sjw0066@naver.com>
2025-02-06 21:53:09 +0900

    0206

--- Sjw0066/SWEA/0206/p5.py
+++ Sjw0066/SWEA/0206/p5.py
@@ -1,27 +1,39 @@
-import sys
-sys.stdin = open("input.txt", "r")
 T = 10
 
 for tc in range(1, T + 1):
     N=int(input())
     boxes=list(map(int,input().split()))
 
+    def bubble_sort(arr):
+        for i in range(len(arr)-1,0,-1):
+            for j in range(0,i):
+                if arr[j] > arr[j+1]:
+                    arr[j],arr[j+1]=arr[j+1],arr[j]
+        return arr
+    
+    def check_break(arr):
+        arr=set(arr)
+        arr=list(arr)
+        if len(arr) < 3:
+            return True
+        else:
+            return False
 
     answer= 0
 
+
     for i in range(N):
-        max_height = -21e8
-        min_height = 21e8
-        for j in range(len(boxes)):
-            if min_height>boxes[j]:
-                min_height = boxes[j]
-                boxes[j] += 1
-
-            if max_height < boxes[j]:
-                max_height= boxes[j]
-                boxes[j] -= 1
-
-    if max_height-min_height>=0:
-        answer=max_height-min_height
-
+        sorted_boxes=bubble_sort(boxes)
+        
+        sorted_boxes[0] += 1
+        sorted_boxes[-1] -= 1
+        
+        flag=check_break(sorted_boxes)
+        if flag :
+            break
+    
+    sorted_boxes = bubble_sort(sorted_boxes)
+    answer = sorted_boxes[-1] - sorted_boxes[0]    
+    
     print(f'#{tc} {answer}')
+    
