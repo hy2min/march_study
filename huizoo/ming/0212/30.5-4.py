8063aad20eba34e1886eb03c6812ead59a88a243
김희주 <ajtwlsgmlwn@naver.com>
2025-02-12 18:00:03 +0900

    ming

---
+++ huizoo/ming/0212/30.5-4.py
@@ -0,0 +1,14 @@
+arr = [input() for _ in range(3)]
+longest = []
+long_len = -1
+for i in range(3):
+    if len(arr[i]) > len(longest):
+        longest = [arr[i]]
+        long_len = len(arr[i])
+    elif len(arr[i]) == len(longest):
+        longest.append(arr[i])
+
+if len(longest) > 1:
+    print(max(longest))
+else:
+    print(*longest)
