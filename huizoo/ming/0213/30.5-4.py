03aebe76664322f1246a84b088c63748318c610e
김희주 <ajtwlsgmlwn@naver.com>
2025-02-13 23:52:28 +0900

    study

--- huizoo/ming/0213/30.5-4.py
+++ huizoo/ming/0213/30.5-4.py
@@ -1,11 +1,11 @@
 arr = [input() for _ in range(3)]
-longest = []
+longest = ['']
 long_len = -1
 for i in range(3):
-    if len(arr[i]) > len(longest):
+    if len(arr[i]) > len(longest[0]):
         longest = [arr[i]]
         long_len = len(arr[i])
-    elif len(arr[i]) == len(longest):
+    elif len(arr[i]) == len(longest[0]):
         longest.append(arr[i])
 
 if len(longest) > 1:
