6490d2854f89a0816d473f9bec75361ce5146fb5
김희주 <ajtwlsgmlwn@naver.com>
2025-03-06 23:58:49 +0900

    study

--- huizoo/SWEA/0306/이진탐색.py
+++ huizoo/SWEA/0306/이진탐색.py
@@ -1,7 +1,16 @@
+def abc(i):
+    global last
+    if i > n :
+        return
+    abc(2*i)
+    arr[i] = last
+    last += 1
+    abc(2*i+1)
+
 t = int(input())
 for tc in range(1, t+1):
-    N = int(input())
-    arr = [0]*(N+1)
-    last = 0
-
-
+    n = int(input())
+    arr = [0]*(n+1)
+    last = 1
+    abc(1)
+    print(f'#{tc} {arr[1]} {arr[n//2]}')
---
+++ huizoo/SWEA/0306/중위순회.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+
+def abc(i):
+    if i > N:
+        return
+    if type(arr[i]) is str:
+        print(arr[i], end='')
+    else:
+        abc(int(arr[i][1]))
+        print(arr[i][0], end='')
+        if len(arr[i]) == 3:
+            abc(int(arr[i][2]))
+
+for tc in range(1, 11):
+    N = int(input())
+    arr = [0]*(N+1)
+    for i in range(1, N+1):
+        temp = list(input().split())
+        if len(temp) == 2:
+            arr[i] = temp[1]
+        else:
+            arr[i] = tuple(temp[1:])
+    print(f'#{tc}', end=' ')
+    abc(1)
+    print()
