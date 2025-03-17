2a49ec8cadb1cab825e5f988aadfd7f784ea7ea5
김희주 <ajtwlsgmlwn@naver.com>
2025-03-17 23:58:10 +0900

    study_baekjoon

---
+++ huizoo/BAEKJOON/0317/주사위_굴리기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+N, M, x, y, K = map(int, input().split())
+
+arr = [list(map(int, input().split())) for _ in range(M)]
+
+order = list(map(int, input().split()))
+
+
