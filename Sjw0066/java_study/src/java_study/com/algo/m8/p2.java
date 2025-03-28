9aa69cae4743f090aa4d172b9fe99c05df9c6e29
신진우 <sjw0066@naver.com>
2025-03-28 17:34:11 +0900

    0328

---
+++ Sjw0066/java_study/src/java_study/com/algo/m8/p2.java
@@ -0,0 +1,38 @@
+package java_study.com.algo.m8;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+
+public class p2 {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr= new int[6];
+		
+		for (int i = 0 ; i < 6 ; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0 ; i <6 ; i++) {
+			if (arr[i] == 7) {
+				break;
+			}else {
+				bw.write(arr[i]+" ");
+			}
+				
+		}
+		
+		
+		bw.close();
+		br.close();
+				
+
+	}
+
+}
