499407ced2b1586c7201cff0aafc1afd8b095c85
huizoo <ajtwlsgmlwn@naver.com>
2025-04-08 17:02:24 +0900

    study

---
+++ huizoo/ming/04/0407/0407/1.5-7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int a = 10;
+	int b = 3;
+	cout << "10 * 3 = " << a * b << endl;
+	cout << "10 / 3 = " << a / b;
+	
+	return 0;
+}
