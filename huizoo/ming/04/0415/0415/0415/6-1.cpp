1d927fdb19daffb610a05aa805005176e717cb69
huizoo <ajtwlsgmlwn@naver.com>
2025-04-15 17:57:22 +0900

    study

---
+++ huizoo/ming/04/0415/0415/0415/6-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+char st1, st2;
+
+void input()
+{
+	cin >> st1 >> st2;
+
+}
+
+void output()
+{
+	cout << st1 << ' ' << st2;
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
