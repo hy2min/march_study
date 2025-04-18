1bfba2023a941e57377f43caf13d7391f01e9785
신진우 <sjw0066@naver.com>
2025-04-18 17:59:00 +0900

    1

---
+++ Sjw0066/java_study/src/swea1288.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class swea1288 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc=1; tc<T+1;tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] dat=new int[10];
+			for (int i =0; i<10; i++) {
+				dat[i]=0;
+			}
+			
+			int k=1;
+			while (true) {
+				String temp=String.valueOf(k*N);
+				
+				for (String t : temp.split("")) {
+					dat[Integer.parseInt(t)] += 1;
+				}
+				
+				boolean flag=true;
+				for (int i =0; i<10; i++) {
+					if (dat[i]==0) {
+						flag=false;
+					}
+				}
+				if (flag) {
+					break;
+				}
+				k++;
+			}
+			
+			bw.write("#"+tc+" "+k*N+"\n");
+		
+		}
+		bw.close();
+		br.close();		
+	}
+}
