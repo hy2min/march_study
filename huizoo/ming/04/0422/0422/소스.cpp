aa30ff300500a9db9ceeaca0841044c8d22d6005
yangdain <didek5123@naver.com>
2025-04-23 15:41:55 +0900

    study

--- huizoo/ming/04/0422/0422/소스.cpp
+++ huizoo/ming/04/0422/0422/소스.cpp
@@ -1,1 +1,31 @@
-// 12-10
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char st[100];
+	cin >> st;
+	char last;
+	int n;
+	for (int x = 0; x < 100; x++) {
+		if (st[x] == '\0') {
+			last = st[x-1];
+			n = x;
+			break;
+		}
+	}
+	int cnt = 0;
+	for (int x = 0; x < n; x++) {
+		if (st[x] == last) {
+			cnt += 1;
+		}
+	}
+	cout << n << endl << cnt;
+
+
+
+
+	return 0;
+}
+
+// 12.5에 6은 풀었지만 12.4, 12.5는 아직 안풀었음 풀어야함
